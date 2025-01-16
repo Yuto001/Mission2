c88bde568e5133b8b37f52e5f4794d73918fca22
Yuto001 <yuto.t1234@gmail.com>
2025-01-15 17:36:17 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Xml.Linq;
+using Mission2;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        int dice = 0;
+
+        StimulateDice sd = new StimulateDice();
+
+        System.Console.WriteLine("Welcom to the dice throwing stimulator!\n");
+        System.Console.WriteLine("How many dice rolls would you like to stimulate?");
+        dice = int.Parse(System.Console.ReadLine());
+
+        sd.CalcDice(dice);
+    }
+}
---
+++ StimulateDice.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Mission2
+{
+    internal class StimulateDice
+    {
+        
+        public void CalcDice(int dice)
+        {
+
+            Dictionary<int, int> rollHistory = new Dictionary<int, int>();
+            for (int i = 2; i <= 12; i++)
+            {
+                rollHistory[i] = 0;
+            }
+
+            Random rand = new Random();
+
+
+            for (int i = 0; i < dice; i++)
+            {
+                int dice1 = rand.Next(1,7);
+                int dice2 = rand.Next(1,7);
+                int sum = dice1 + dice2;
+
+                rollHistory[sum]++;
+            }
+
+         Console.WriteLine("DICE ROLLING STIMULATION RESULT");
+         Console.WriteLine("Each '*' represents 1% of the total numbers of rolls.");
+         Console.WriteLine("Total number of rolls = " + dice + ".\n");
+
+            for (int sum = 2; sum <= 12; sum++) // Loop from 2 to 12
+            {
+                int count = rollHistory[sum];
+
+                // Calculate percentage
+                double percentage = (double)count / dice * 100;
+
+                // Create asterisks representation (1% = 1 *)
+                string stars = new string('*', (int)Math.Round(percentage));
+
+                // Display the sum, percentage, and stars
+                Console.WriteLine($"{sum}: {stars}");
+            }
+
+            Console.WriteLine("\nThank you for using the dice throwing stimulator. Goodbye!");
+
+        }
+        
+    }
+}
